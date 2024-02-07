7cb06aa2b96e6207d1e7cd621f29fbc0860a836b
RohitIndus <upadhyay.rohit@indusind.com>
2024-02-07 17:08:57 +0530

    App Now work on a button

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-console.log("Hello World");
 let json_output;
 let hostname = []
 let consumed_units = []
 let temp_var = []
 let start_table_creation=[];
 
-Func();
 
 
-function Func() { 
-	fetch("https://apm.indusind.com/e/a5524478-859f-46f3-abcf-9f33bb51d130/api/v1/oneagents?includeDetails=true&availabilityState=MONITORED",{
+
+function Func(a,callback) { 
+  fetch("https://apm.indusind.com/e/a5524478-859f-46f3-abcf-9f33bb51d130/api/v1/oneagents?includeDetails=true&availabilityState=MONITORED",{
   method: "GET",
   headers: {"Authorization": "Api-Token dt0c01.OCEZZ7BO63YFVA3R2YCDX35Y.OHTMAWBAEUXSZ4GH6G4BQEJDBYJHAXUXFMDZUAMVLDAKJJ27YOLNNBO7KXV3CK2E"}
 }) 
@@ -17,7 +16,6 @@ function Func() {
 		return res.json(); 
 	})  
 	.then((data) => {
-        console.log(data);
         json_output = data;
         console.log(json_output);
 
@@ -25,43 +23,55 @@ function Func() {
             {
                 hostname[i] = json_output.hosts[i].hostInfo.displayName;
                 consumed_units[i] = json_output.hosts[i].hostInfo.consumedHostUnits
-               // document.querySelector("td").innerHTML=json_output.hosts[i];
-              //  console.log(hostname[i],consumed_units[i]);
                 hostname.unshift(hostname[i]);
                 consumed_units.unshift(consumed_units[i]);
                 start_table_creation.unshift("1");
-                
+               //console.log(hostname[i]);   
             }
-
-            
-       
-    });
-console.log("Executed Func Sucess" +start_table_creation );   
+        
+            callback();
+            sum();
+        });
+    
 }
 
-console.log(hostname);
-console.log(consumed_units);
-
-
-let sleep = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-    };
-    
-    //document.write("Begin" + "<br>");
-    console.log("Sleeping");
-    sleep(5000).then(() => 
-    {
-        if(start_table_creation)
-        {
-        table_creation();
-        }
-
-    });
-
-function table_creation ()
+function sum()
 {
+    const sum = consumed_units.reduce((partialSum, a) => partialSum + a, 0);
+    console.log(sum);
+    document.getElementById("myButton1").innerHTML=sum;
+}
+
+//----------SLEEP
+// let sleep = ms => {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//     };
+    
+//     //document.write("Begin" + "<br>");
+//     console.log("Sleeping");
+//     sleep(5000).then(() => 
+//     {
+//         console.log("Done Sleeping");
+//         if(start_table_creation.includes("1"))
+//         {
+//        // console.log("Inside Table Creation Logic" +start_table_creation );   
+//         table_creation();
+//         }
+
+//     });
+
+//------------------------------------------------------------------------
+
+
+
+function table_creation()
+{
+    console.log("table_creation ini");
+    console.log(hostname.length);
     var table = document.createElement('table');
-    for (var i = 1; i < hostname.length; i++){
+    for (var i = 1; i < hostname.length; i++)
+    {
+   
     var tr = document.createElement('tr');   
 
     var td1 = document.createElement('td');
@@ -79,3 +89,5 @@ function table_creation ()
 }
 document.body.appendChild(table);
 }
+
+//Func(1,table_creation);
